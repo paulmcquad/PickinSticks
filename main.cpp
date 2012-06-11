b2bc5173f26a98d73b8fc3a6f63bcc22e29bdec2
Paul McQuade <paulmcquad@gmail.com>
2012-06-11 15:14:40 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <SDL/SDL.h>
+#include "main.hpp"
+
+using namespace std;
+
+int main ( int argc, char* argv[] )
+{
+    Main main;
+    main.Setup(); // Init
+
+    //Quit SDL
+    SDL_Quit();
+
+     return 0;
+}
---
+++ main.hpp
@@ -0,0 +1,43 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+class Main
+{
+    private:
+        int screenWidth, screenHeight;
+        bool fullscreen;
+
+    public:
+//        System();
+//        ~System();
+        int Setup();
+
+        SDL_Surface *buffer;
+};
+
+
+int Main::Setup()
+{
+       //Start SDL
+    SDL_Init( SDL_INIT_EVERYTHING );
+    SDL_WM_SetCaption("Pickin Sticks", NULL);
+
+    //The images
+    bool fullscreen = false;
+
+    if (fullscreen == true)
+        buffer = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE | SDL_FULLSCREEN );
+    else
+        buffer = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE);
+
+    SDL_Event event;
+
+    while (event.type != SDL_QUIT)
+    {
+        SDL_PollEvent ( &event);
+        SDL_Flip (buffer);
+    }
+     return 1;
+}
+
+#endif // MAIN_HPP
