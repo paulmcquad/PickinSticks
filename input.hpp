ffc36089cf6688a667d8c3815aee36c80325823f
Paul McQuade <paulmcquad@gmail.com>
2012-06-12 21:21:06 +0100

    3rd commit

---
+++ input.hpp
@@ -0,0 +1,42 @@
+#ifndef INPUT_HPP
+#define INPUT_HPP
+
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+
+class input
+{
+    Uint8 *key;
+    public:
+     void GetInput();
+
+};
+
+enum Direction { UP = 0, DOWN = 1,
+                LEFT = 2, RIGHT = 3};
+
+void input::GetInput()
+{
+/*
+    while (Game.Done() == false )
+    {
+        key = SDL_GetKeyState( NULL);
+        if ( key {SDLK_UP})
+            player.Move(UP);
+        else if ( key {SDLK_DOWN})
+            player.Move(DOWN);
+            if ( key {SDLK_LEFT})
+            player.Move(LEFT);
+        else if (key {SDLK_RIGHT})
+            player.Move(RIGHT);
+
+    }
+            if ( key[SDLK_RIGHT] && key[SDLK_UP])
+            {
+                player.HalfSpeed();
+                player.Move( RIGHT );
+                player.Move( UP );
+            }*/
+}
+
+#endif // INPUT_HPP
---
+++ player.hpp
@@ -0,0 +1,35 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+class Player
+{
+  SDL_Surface *buffer;
+  SDL_Surface *image;
+
+  SDL_Rect crop;
+  SDL_Rect location;
+
+  public:
+    int player();
+
+};
+
+int Player::player()
+{
+  //  image = IMG_Load("PSVIII_Moosader.png");
+  //  image = SDL_LoadBMP("PeachKat_Moosader.bmp");
+
+    Uint32 colorkey = SDL_MapRGB(image->format, 0xFF, 0, 0xFF);
+
+    SDL_SetColorKey(image, SDL_SRCCOLORKEY, colorkey);
+
+    crop.x = 0, crop.y = 0;
+    crop.w = 32; crop.h = 32;
+    location.x = 0; location.y = 0;
+    location.w = 32; location.h = 32;
+
+    SDL_BlitSurface (image, &crop, buffer, &location);
+    return 1;
+}
+
+#endif // PLAYER_HPP
